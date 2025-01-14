dff97e3a4612688f4f5c8fdf90175d96b9a13655
praveem <praveen2272000.com>
2025-01-15 02:05:26 +0530

    phase2commit

--- src/components/BackgroundImage.jsx
+++ src/components/BackgroundImage.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 function BackgroundImage() {
   return (
     <div className="bg">
-      <img className="bgm" src="/Images/solo12.png" alt="Background" />
+      <img className="bgm" src="./Images/solo12.png" alt="Background" />
     </div>
   );
 }
